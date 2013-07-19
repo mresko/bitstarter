03ad4a5511cdb4049e44c0e0c288208983e92a67
Siim Esko <siim.esko@gmail.com>
2013-07-19 07:05:04 +0000

    Modified index.html

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var body = body
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +66,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <html_file>', 'url to index.html', HTMLFILE_DEFAULT)
+		.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
